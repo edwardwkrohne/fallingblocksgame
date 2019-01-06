342d8e74bbdef7bdc535cb317a6ac86f4d5f9d4b
Ed Krohne <krohneew@gmail.com>
2019-01-06 15:28:58 -0600

    Fixed mistakes with the tests.

--- block.cpp
+++ block.cpp
@@ -3,19 +3,8 @@
 using namespace std;
 
 void rotateCW(char (&block)[5][5]) {
-    char new_block[5][5];
 
-    for ( int i = 0; i < 5; i++ ) {
-        for ( int j = 0; j < 5; j++ ) {
-            new_block[j][4-i] = block[i][j];
-        }
-    }
 
-    for ( int i = 0; i < 5; i++ ) {
-        for ( int j = 0; j < 5; j++ ) {
-            block[i][j] = new_block[i][j];
-        }
-    }
 }
 
 void printBlock(const char (&block)[5][5], ostream& out) {
