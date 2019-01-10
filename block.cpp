2d186a31974918e2ef6390e677add064d7fb0012
Divin <divin.sarthak@gmail.com>
2019-01-10 15:10:01 -0600

    Implemented rotateCW

--- block.cpp
+++ block.cpp
@@ -2,16 +2,28 @@
 
 using namespace std;
 
+
 void rotateCW(char (&block)[5][5]) {
+  char temp_block[5][5];
+  for (int i =0;i < 5; i++){
+    for (int j =0; j < 5; j++){
 
+      temp_block[j][4-i]=  block[i][j] ;
+    }
+  }
+  for (int i =0;i < 5; i++){
+    for (int j =0; j < 5; j++){
+      block[i][j] = temp_block[i][j];
 
+    }
+  }
 }
 
 void printBlock(const char (&block)[5][5], ostream& out) {
-    for ( int i = 0; i < 5; i++ ) {
-        for ( int j = 0; j < 5; j++ ) {
-            out << block[i][j] << " ";
-        }
-        out << endl;
+  for ( int i = 0; i < 5; i++ ) {
+    for ( int j = 0; j < 5; j++ ) {
+      out << block[i][j] << " ";
     }
+    out << endl;
+  }
 }
--- block.h
+++ block.h
@@ -3,7 +3,9 @@
 #ifndef BLOCK_H_INCLUDED
 #define BLOCK_H_INCLUDED
 
+
 void rotateCW(char (&block)[5][5]);
+
 
 void printBlock(const char (&block)[5][5], std::ostream& out = std::cout);
 
