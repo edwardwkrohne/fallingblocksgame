ff79e15e9d747c6a89e54abb31e9ffed2a9925c6
Ed Krohne <krohneew@gmail.com>
2019-01-10 18:02:14 -0600

    Added unit tests and prototypes for rotateCCW, as requested.

--- block.cpp
+++ block.cpp
@@ -7,6 +7,11 @@ void rotateCW(char (&block)[5][5]) {
 
 }
 
+void rotateCCW(char (&block)[5][5]) {
+
+
+}
+
 void printBlock(const char (&block)[5][5], ostream& out) {
     for ( int i = 0; i < 5; i++ ) {
         for ( int j = 0; j < 5; j++ ) {
--- block.h
+++ block.h
@@ -4,6 +4,7 @@
 #define BLOCK_H_INCLUDED
 
 void rotateCW(char (&block)[5][5]);
+void rotateCCW(char (&block)[5][5]);
 
 void printBlock(const char (&block)[5][5], std::ostream& out = std::cout);
 
